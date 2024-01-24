b8b871d610cdf8969818d7577d46ca096c7ad33c
nkorn1lov <nikolai.kornilov@protofire.io>
2024-01-24 03:48:35 +0300

    feat: update

--- oracle/src/storage_types.rs
+++ oracle/src/storage_types.rs
@@ -1,7 +1,8 @@
 use soroban_sdk::{contracttype, String};
 
 pub(crate) const DAY_IN_LEDGERS: u32 = 17280;
-pub(crate) const VALUE_TTL_BUMP: u32 = 7 * DAY_IN_LEDGERS;
+pub(crate) const VALUE_BUMP_AMOUNT: u32 = 7 * DAY_IN_LEDGERS;
+pub(crate) const VALUE_LIFETIME_THRESHOLD: u32 = VALUE_BUMP_AMOUNT - DAY_IN_LEDGERS;
 
 #[contracttype]
 #[derive(Clone)]
